a2c44b21ae88a7cffe45ca66c2a75a559895f2db
Marco Montel <marco.montel@syncromed.it>
2015-02-17 10:09:16 +0100

    RestConfiguration relocated

--- master-detail-json/src/main/java/org/samples/enterprise/masterdetail/business/RestConfiguration.java
+++ master-detail-json/src/main/java/org/samples/enterprise/masterdetail/business/RestConfiguration.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.samples.enterprise.masterdetail.business.service;
+package org.samples.enterprise.masterdetail.business;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
